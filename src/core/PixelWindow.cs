9080471e4bc3423e974db2e6de9107e501148c8a
Jakub <jakubshark@gmail.com>
2019-06-24 11:27:30 -0700

    Added Comments

--- src/core/PixelWindow.cs
+++ src/core/PixelWindow.cs
@@ -1,11 +1,33 @@
 namespace Szark
 {
+    /// <summary>
+    /// This window is a Window for drawing pixel graphics,
+    /// easily with the CPU. All you need to do is inherit
+    /// from it and implement the required methods.
+    /// </summary>
     public abstract class PixelWindow
     {
+        /// <summary>
+        /// Width of the Screen in Pixels,
+        /// Use this over Window.Width
+        /// </summary>
         public int ScreenWidth { get; private set; }
+
+        /// <summary>
+        /// Height of the Screen in Pixels,
+        /// Use this over Window.Height
+        /// </summary>
         public int ScreenHeight { get; private set; }
+
+        /// <summary>
+        /// Density / Size of each Pixel
+        /// </summary>
         public int PixelSize { get; private set; }
 
+        /// <summary>
+        /// Graphics2D Object for Drawing to the Screen,
+        /// Use this in Render Method
+        /// </summary>
         public Graphics2D GFX { get; private set; }
         
         private Texture drawTarget;
@@ -49,9 +71,28 @@ namespace Szark
             Window.Create(title, width, height);
         }
 
+        /// <summary>
+        /// Called when the Window opens,
+        /// Use this for initialization
+        /// </summary>
         public abstract void Start();
+
+        /// <summary>
+        /// Runs every tick,
+        /// Use this for logic
+        /// </summary>
         public abstract void Update(float deltaTime);
+
+        /// <summary>
+        /// Runs every frame,
+        /// Use this for drawing
+        /// </summary>
         public abstract void Render(float deltaTime);
+
+        /// <summary>
+        /// Called when Window closes,
+        /// Use this for cleanup
+        /// </summary>
         public abstract void Destroyed();
     }
 }
