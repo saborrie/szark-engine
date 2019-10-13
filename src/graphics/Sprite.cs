5b96c7f10ee3eebcbd6cdf8d3cb9f831c27b9c5c
Jakub <jakubshark@gmail.com>
2019-10-13 09:41:58 -0700

    Removed Extensions, Moved to Respective Classes

--- src/graphics/Sprite.cs
+++ src/graphics/Sprite.cs
@@ -1,5 +1,6 @@
 using System;
 using OpenTK.Graphics.OpenGL4;
+using OpenTK;
 
 namespace Szark
 {
@@ -7,6 +8,8 @@ namespace Szark
     {
         public readonly int id;
         public readonly int width, height;
+
+        private const float Clipping = 100f;
 
         public Shader Shader { get; set; }
 
@@ -39,6 +42,70 @@ namespace Szark
         {
             GL.DeleteTexture(id);
             GC.SuppressFinalize(this);
+        }
+
+        public void Render(Transform transform, Action extra = null)
+        {
+            GL.UseProgram(Shader.id);
+
+            GetMVP(this, transform, out var mvp);
+
+            // Send matrices to the shader
+            GL.UniformMatrix4(Shader.mvpLocation, false, ref mvp);
+
+            // Bind texture the the shader
+            GL.BindTexture(TextureTarget.Texture2D, id);
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, QuadData.EBO);
+            GL.BindVertexArray(QuadData.VAO);
+
+            extra?.Invoke();
+
+            // Draw the sprite
+            GL.DrawElements(PrimitiveType.Triangles,
+                6, DrawElementsType.UnsignedInt, 0);
+        }
+
+        public void RenderBatch(Transform[] transforms, Action<int> extra = null)
+        {
+            GL.UseProgram(Shader.id);
+
+            // Bind texture the the shader
+            GL.BindTexture(TextureTarget.Texture2D, id);
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, QuadData.EBO);
+            GL.BindVertexArray(QuadData.VAO);
+
+            for (int i = 0; i < transforms.Length; i++)
+            {
+                GetMVP(this, transforms[i], out var mvp);
+
+                // Send matrices to the shader
+                GL.UniformMatrix4(Shader.mvpLocation, false, ref mvp);
+
+                extra?.Invoke(i);
+
+                // Draw the sprite
+                GL.DrawElements(PrimitiveType.Triangles,
+                    6, DrawElementsType.UnsignedInt, 0);
+            }
+        }
+
+        private static void GetMVP(Sprite sprite, Transform transform, out Matrix4 mvp)
+        {
+            float ratioX = sprite.width * 0.5f, ratioY = sprite.height * 0.5f;
+            float posX = transform.position.x / ratioX, posY = transform.position.y / ratioY;
+
+            mvp = Matrix4.Identity;
+
+            if (transform.rotation != 0)
+                mvp *= Matrix4.CreateRotationZ(transform.rotation);
+
+            mvp *= Matrix4.CreateTranslation(posX + 1, posY + 1, transform.layer / Clipping);
+            
+            if (transform.scale != 1)
+                mvp *= Matrix4.CreateScale(transform.scale, transform.scale, 1);
+
+            mvp *= Matrix4.CreateOrthographicOffCenter(0, Window.Width / ratioX,
+                Window.Height / ratioY, 0, -Clipping, Clipping);
         }
     }
 }
--- src/util/Vector.cs
+++ src/util/Vector.cs
@@ -48,6 +48,27 @@ namespace Szark
         public Vector Scale(Vector other) =>
             new Vector(x * other.x, y * other.y);
 
+        // -- Static Methods --
+
+        /// <summary>
+        /// Linearly interpolates a -> b based on the time.
+        /// </summary>
+        public static Vector Lerp(Vector a, Vector b, float time) =>
+            new Vector(Mathf.Lerp(a.x, b.x, time), Mathf.Lerp(a.y, b.y, time));
+
+        /// <summary>
+        /// Returns the distance between a and b.
+        /// </summary>
+        public static float Distance(Vector a, Vector b) =>
+            (float)Math.Sqrt((b.x - a.x) * (b.x - a.x) + 
+                (b.y - a.y) * (b.y - a.y));
+
+        /// <summary>
+        /// Returns the inner product of the two vectors.
+        /// </summary>
+        public static float Dot(Vector a, Vector b) =>
+            a.x * b.x + a.y * b.y;
+
         // -- Overloaded Arithmetic Operators --
 
         public static Vector operator +(Vector a, Vector b) =>
